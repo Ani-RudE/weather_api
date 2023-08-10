968421d2e57444f236d7b748969ff158d2435b66
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-08-10 06:26:34 +0530

    main

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Website Link - https://ani-rude.github.io/weather_api/
 const apiKey="863242cfb2b1d357e6093d9a4df19a4b";
 const apiURL="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox=document.querySelector(".search input");
